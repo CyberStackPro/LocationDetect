aa79fbd213c15ca6bf14775fe7e769add455d57b
CyberStackPro <yeabsragebriel@gmail.com>
2024-11-16 14:50:34 +0300

    change url

--- src/pages/LocationRegistration.tsx
+++ src/pages/LocationRegistration.tsx
@@ -24,8 +24,8 @@ export interface FormData {
 }
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
-  // baseURL: "https://backendlocation-gmb4.onrender.com/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://backendlocation-gmb4.onrender.com/api",
   timeout: 30000,
   headers: {
     "Content-Type": "application/json",
